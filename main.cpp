8ba5fb05aea27e08fb4e3249a306c7c6195f089f
Soawii <65302714+Soawii@users.noreply.github.com>
2025-06-17 20:42:57 +0300

    Taken word frequency into account, improved speed

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <queue>
 #include <set>
 #include <iomanip>
+#include <unordered_map>
 
 enum Letter {
     GRAY = 0,
@@ -42,7 +43,15 @@ std::vector<std::string> original_words;
 std::vector<std::pair<std::string, int>> words_copy, words;
 std::vector<std::array<int8_t, 26>> word_to_letter;
 std::array<Permutation, 243> all_permutations;
+std::vector<double> word_frequency;
 int permutation_index = 0;
+
+void trimStart(std::string& s) {
+    int front_spaces = 0;
+    for (int i = 0; i < s.size() && s[i] == ' '; i--)
+        front_spaces++;
+    s = s.substr(front_spaces, 5);
+}
 
 void loadWords(std::string filename, std::vector<std::string>& where) {
     std::fstream file;
@@ -53,6 +62,32 @@ void loadWords(std::string filename, std::vector<std::string>& where) {
             where.push_back(word);
         }
         file.close();
+    }
+}
+
+void loadFrequency(int CENTER, double X_COEFF) {
+    std::vector<std::string> words_by_frequency;
+    std::fstream file;
+    {
+        file.open("word_frequency.txt", std::ios::in);
+        std::string word, frequency;
+        while (file >> word >> frequency) {
+            words_by_frequency.push_back(word);
+        }
+        file.close();
+    }
+
+    std::unordered_map<std::string, size_t> word_to_index;
+    for (int i = 0; i < original_words.size(); i++) {
+        word_to_index[original_words[i]] = i;
+    }
+    const double MIN_VALUE = 0.00001;
+    word_frequency.resize(original_words.size(), 0);
+
+    for (int i = 0; i < word_frequency.size(); i++) {
+        double X = CENTER - i;
+        double sigmoid = std::max(MIN_VALUE, double(1) / (double(1) + double(exp(-X_COEFF * X))));
+        word_frequency[word_to_index[words_by_frequency[i]]] = sigmoid; 
     }
 }
 
@@ -203,23 +238,23 @@ int getPermutationIdx(int word1, int word2) {
 }
 
 double getEntropyForWord(int word) {
-    std::array<int, 243> permutation_count;
+    std::array<double, 243> permutation_count;
     permutation_count.fill(0);
 
     for (int i = 0; i < words.size(); i++) {
         int p_idx = getPermutationIdx(word, words[i].second);
-        permutation_count[p_idx]++;
+        permutation_count[p_idx] += word_frequency[words[i].second];
     }
 
-    int total_count = 0;
+    double total_sum = 0;
     for (int i = 0; i < 243; i++) 
-        total_count += permutation_count[i];
+        total_sum += permutation_count[i];
 
     double entropy = 0;
     for (int i = 0; i < 243; i++) {
         if (permutation_count[i] == 0)
             continue;
-        double match_percent = double(permutation_count[i]) / double(total_count);
+        double match_percent = double(permutation_count[i]) / double(total_sum);
         entropy -= match_percent * log2(match_percent);
     }
 
@@ -228,11 +263,18 @@ double getEntropyForWord(int word) {
 
 int simulateGame(std::string guessed_word) {
     const int N = original_words.size();
-    const int N_4 = N / 4;
+
     resetWords();
     std::vector<double> entropies(original_words.size(), 0);
+    std::vector<bool> stillIn(original_words.size(), true);
+
     for (int g = 0; g < 6; g++)
     {
+        double sum_probabilities = 0;
+        for (int i = 0; i < words.size(); i++) {
+            sum_probabilities += word_frequency[words[i].second];
+        }
+
         std::string best_guess = "";
         if (g != 0) {
             #pragma omp parallel for
@@ -241,8 +283,10 @@ int simulateGame(std::string guessed_word) {
             }
             double best_entropy = 0;
             for (int i = 0; i < entropies.size(); i++) {
-                if (entropies[i] > best_entropy) {
-                    best_entropy = entropies[i];
+                double probability = (stillIn[i] ? (word_frequency[i] / sum_probabilities) : 0.0f);
+                double new_entropy = (stillIn[i] ? (entropies[i] + 1.5 * pow(probability, 2)) : entropies[i]);
+                if (new_entropy > best_entropy) {
+                    best_entropy = new_entropy;
                     best_guess = original_words[i];
                 }
             }
@@ -252,14 +296,15 @@ int simulateGame(std::string guessed_word) {
 
         Permutation p = getPermutation(best_guess, guessed_word);
 
+        std::for_each(words.begin(), words.end(), [&best_guess, &p, &stillIn](const std::pair<std::string, int>& pair){
+            if (!doesMatch(best_guess, pair.first, p)) {
+                stillIn[pair.second] = false;
+            }
+        });
         auto new_end = std::remove_if(words.begin(), words.end(), [&best_guess, &p](const std::pair<std::string, int>& pair) {
             return !doesMatch(best_guess, pair.first, p);
         });
-
-        int to_delete = words.end() - new_end;
-        while (to_delete--) {
-            words.pop_back();
-        }
+        words.erase(new_end, words.end());
 
         if (words.size() == 1) {
             if (words[0].first != best_guess) {
@@ -271,26 +316,30 @@ int simulateGame(std::string guessed_word) {
     return -1;
 }
 
-void simulateGames() {
+void simulateGames(std::string filenameWithWords) {
     std::vector<std::string> answers;
-    loadWords("answer_words.txt", answers);
+    loadWords(filenameWithWords, answers);
 
     std::cout << "Starting the simulation\n";
     int total_guesses_taken = 0;
     int correct_guesses = 0;
 
     for (int i = 0; i < answers.size(); i++) {
-        std::cout << i << " games processed...\n";
+        if (i % 100 == 0)
+            std::cout << i << "/" << answers.size() << " games processed...\n";
         int guesses_taken = simulateGame(answers[i]);
         if (guesses_taken != -1) {
             total_guesses_taken += guesses_taken;
             correct_guesses++;
         }
+        else {
+            total_guesses_taken += 6;
+        }
     }
 
-    std::cout << "Correct guesses - " << correct_guesses << "/" << answers.size() << std::endl;
-    std::cout << "Correct percent - " << double(correct_guesses) / double(answers.size()) << std::endl;
-    std::cout << "Average guesses - " << double(total_guesses_taken) / double(correct_guesses) << std::endl;
+    std::cout << "Words guessed correctly       - " << correct_guesses << "/" << answers.size() << std::endl;
+    std::cout << "Correct word percentage       - " << double(correct_guesses) / double(answers.size()) << std::endl;
+    std::cout << "Average guess count per word  - " << double(total_guesses_taken) / double(correct_guesses) << std::endl;
 }
 
 void playGame() {
@@ -310,6 +359,11 @@ void playGame() {
             break;
         }
 
+        double sum_probabilities = 0;
+        for (int i = 0; i < words.size(); i++) {
+            sum_probabilities += word_frequency[words[i].second];
+        }
+
         std::cout << "Processing " << words.size() << " words..." << std::endl;
         std::vector<Guess> best_overall_guesses, best_possible_guesses; 
 
@@ -321,10 +375,11 @@ void playGame() {
 
             std::priority_queue<Guess, std::vector<Guess>, GuessCompare> overall_pq, possible_pq;
             for (int i = 0 ; i < entropies.size(); i++) {
-                double probability = (stillIn[i] ? (1.0f / words.size()) : 0.0f);
-                overall_pq.push({entropies[i], probability, i});
+                double probability = (stillIn[i] ? (word_frequency[i] / sum_probabilities) : 0.0f);
+                double new_entropy = (stillIn[i] ? (entropies[i] + 1.5 * pow(probability, 1.5)) : entropies[i]);
+                overall_pq.push({new_entropy, probability, i});
                 if (stillIn[i])
-                    possible_pq.push({entropies[i], probability, i});
+                    possible_pq.push({new_entropy, probability, i});
                 if (overall_pq.size() > BEST_GUESS_PRINT_AMOUNT)
                     overall_pq.pop();
                 if (possible_pq.size() > BEST_GUESS_PRINT_AMOUNT)
@@ -343,39 +398,33 @@ void playGame() {
             reverse(best_possible_guesses.begin(), best_possible_guesses.end());
         }
         else {
-            std::fstream file;
-            {
-                file.open("best_first_guess.txt", std::ios::in);
-                std::string word, entropy;
-                double probability = 1.0f / words.size();
-                while (file >> word >> entropy) {
-                    best_overall_guesses.push_back(Guess{std::stod(entropy), probability, int(std::find(original_words.begin(), original_words.end(), word) - original_words.begin())});
-                }
-                file.close();
-            }
-            while(best_overall_guesses.size() > BEST_GUESS_PRINT_AMOUNT) {
-                best_overall_guesses.pop_back();
-            }
+            best_overall_guesses.push_back({6.32, 0.0001, 12810});
+            best_overall_guesses.push_back({6.25, 0.0001, 6691});
+            best_overall_guesses.push_back({6.22, 0.0003, 10374});
+            best_overall_guesses.push_back({6.20, 0.0002, 10303});
+            best_overall_guesses.push_back({6.20, 0.0003, 12886});
             best_possible_guesses = best_overall_guesses;
         }
 
         std::cout << std::setiosflags(std::ios::left | std::ios::fixed);
         std::cout << std::setw(45) << "\nBest Overall Guesses" << std::setw(45) << "Best Possible Guesses" << std::endl;
-        std::cout << std::setw(15) << "[WORD]" << std::setw(15) << "[ENTROPY]" << std::setw(15) << "[%]" << std::setw(15) << "[WORD]" << std::setw(15) << "[ENTROPY]" << std::setw(15) << "[%]" << std::endl;
+        std::cout << std::setw(15) << "[WORD]" << std::setw(15) << "[SCORE]" << std::setw(15) << "[%]" << std::setw(15) << "[WORD]" << std::setw(15) << "[SCORE]" << std::setw(15) << "[%]" << std::endl;
         for (int i = 0; i < best_overall_guesses.size(); i++) {
-            std::cout << std::setw(15) << original_words[best_overall_guesses[i].word_index] << std::setw(15) << best_overall_guesses[i].entropy << std::setw(15) << std::setprecision(2) << 100 * best_overall_guesses[i].probability;
+            std::cout << std::setprecision(2) << std::setw(15) << original_words[best_overall_guesses[i].word_index] << std::setw(15) << best_overall_guesses[i].entropy << std::setw(15) << 100 * best_overall_guesses[i].probability;
             if (best_possible_guesses.size() > i)
-                std::cout << std::setw(15) << original_words[best_possible_guesses[i].word_index] << std::setw(15) << best_possible_guesses[i].entropy << std::setw(15) << std::setprecision(2) << 100 * best_possible_guesses[i].probability;
+                std::cout << std::setw(15) << original_words[best_possible_guesses[i].word_index] << std::setw(15) << best_possible_guesses[i].entropy << std::setw(15) << 100 * best_possible_guesses[i].probability;
             std::cout << std::endl;
         }
 
         std::cout << std::setw(25) << "Enter your guess word -";
         std::string guess_word;
         std::cin >> guess_word;
+        trimStart(guess_word);
 
         std::cout << std::setw(25) << "Enter guess result -";
         std::string guess_result;
         std::cin >> guess_result;
+        trimStart(guess_result);
         
         if (guess_result == "22222") {
             break;
@@ -385,7 +434,6 @@ void playGame() {
         for (int i = 0; i < 5; i++) {
             p.letter[i] = Letter(guess_result[i] - '0');
         }
-
 
         int le_temp = doesMatch(guess_word, "about", p);
         std::cout << "Excluding incorrect words..." << std::endl;
@@ -402,31 +450,50 @@ void playGame() {
     }
 }
 
+struct Result {
+    double avg_guess;
+    int center;
+    double coeff;
+};
 
-int main() {
-    loadWords("valid_words.txt", original_words);
-    fillPermutations();
-    fillWordToLetter();
-
-    while (true) {
-        playGame();
-        std::cout << "Play again? y/n - ";
-        char answer;
-        while(true)
-        {
-            std::cin >> answer;
-            if (answer == 'y' || answer == 'n')
-                break;
-            else
-                std::cout << "Unknown command, try again - ";
-        }
-        if (answer == 'y')
-            continue;
-        else {
-            std::cout << "Thanks for playing!\n";
-            break;
+int main(int argc, char **argv) {
+    bool simulation = false;
+    if (argc == 2) {
+        if (std::string(argv[1]) == "-sim") {
+            simulation = true;
         }
     }
+
+    loadWords("valid_words.txt", original_words);
+    loadFrequency(3400, 0.0029);
+    fillPermutations();
+    fillWordToLetter();
+    
+    if (simulation) {
+        simulateGames("answer_words.txt");
+    }
+    else {
+        while (true) {
+            playGame();
+            std::cout << "Play again? y/n - ";
+            char answer;
+            while(true)
+            {
+                std::cin >> answer;
+                if (answer == 'y' || answer == 'n')
+                    break;
+                else
+                    std::cout << "Unknown command, try again - ";
+            }
+            if (answer == 'y')
+                continue;
+            else {
+                std::cout << "Thanks for playing!\n";
+                break;
+            }
+        }
+    }
+    
 
     return 0;
 }
